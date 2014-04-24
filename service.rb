126ec6532328f6fc1d59cf7643c1e4d095654314
Todd Guerra <tguerra@crownpartners.com>
2014-04-24 10:39:27 -0400

    a test

--- service.rb
+++ service.rb
@@ -5,25 +5,26 @@ require 'net/http'
 require 'json'
 
 fakeDB = {
-	'testing1' => 'all',
-	'testing2' => 'mining',
-	'testing3' => 'truck'
+  'testing1' => 'all',
+  'testing2' => 'mining',
+  'testing3' => 'truck'
 }
 
 get '/user/:name' do
-	name = params[:name]
-	permission = fakeDB[name]
+  name = params[:name]
+  permission = fakeDB[name]
 
-	content_type :json
-    { :name => name, :permissions => permission }.to_json
-end 
-
-post '/login' do
-	data = JSON.parse(request.body.read)
-
-	name = data["name"]
-	permission = fakeDB[name]
-	
-	{ :name => name, :permissions => permission }.to_json
+  content_type :json
+  { :name => name, :permissions => permission }.to_json
 end
 
+post '/login' do
+  data = JSON.parse(request.body.read)
+
+  name = data["name"]
+  permission = fakeDB[name]
+
+  { :name => name, :permissions => permission }.to_json
+end
+
+#just a test
